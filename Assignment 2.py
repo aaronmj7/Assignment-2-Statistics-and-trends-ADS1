4a7e747b259ef79d4f6ab1c67d73fab762e406be
Aaron M Joseph <125889154+aaronmj7@users.noreply.github.com>
2023-03-27 11:07:20 +0100

    Add files via upload

--- Assignment 2.py
+++ Assignment 2.py
@@ -8,3 +8,11 @@ Created on Mon Mar 13 00:25:50 2023
 import pandas as pd
 import matplotlib.pyplot as plt
 
+df = pd.read_csv("electricity from coal.csv", skiprows=4)
+df.drop(["Country Code", "Indicator Code", "Indicator Name"], axis=1,
+        inplace=True)
+df.dropna(how='all', axis=1, inplace=True)
+df.set_index('Country Name', inplace=True)
+df.columns.name = "Years"
+df_trans = df.T
+print(df, df_trans)
