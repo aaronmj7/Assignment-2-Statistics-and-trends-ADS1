bb3a98298dcef875f0dfceff76754460c2a563ee
Aaron M Joseph <125889154+aaronmj7@users.noreply.github.com>
2023-03-29 02:24:47 +0100

    Add files via upload

--- Assignment 2.py
+++ Assignment 2.py
@@ -15,20 +15,32 @@ def dataframe(file_name, countries, years):
             inplace=True)
     df.dropna(how='all', axis=1, inplace=True)
     df.set_index(['Country Name'], inplace=True)
+    df.index.rename("Country", inplace=True)
     df.columns.name = "Years"
     df = df.loc[countries, years]
     df_t = df.transpose()
     return df, df_t
 
 
-countries = ["South Africa", "India", "Japan", "Australia", "China",
-             "United States", "Russian Federation", "United Kingdom",
-             "Brazil", "Canada", "France", "Sweden"]
+countries = ["India", "Japan", "Australia", "China", "United States",
+             "Russian Federation", "United Kingdom"]
 years = [str(i) for i in range(1990, 2016)]
 df, df_t = dataframe("electricity from oil,gas,coal.csv", countries, years)
 df_t.plot()
 plt.show()
 
-co, co_t = dataframe("co2 emission.csv", countries, years)
+co, co_t = dataframe("co2 emission per capita.csv", countries, years)
 co_t.plot()
+plt.show()
+
+years = [str(i) for i in range(1990, 2016, 5)]
+
+nu, nu_t = dataframe("electricity from nuclear.csv", countries, years)
+nu.plot.bar()
+plt.show()
+
+rn, rn_t = dataframe("Electricity from renewable ex hydro.csv", countries,
+                     years)
+hy, hy_t = dataframe("Electricity from hydro.csv", countries, years)
+rn.add(hy).plot.bar()
 plt.show()
