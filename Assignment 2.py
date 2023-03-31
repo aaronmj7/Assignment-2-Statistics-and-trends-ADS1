a50a3e6b7818ec5299bdaa3697fe454477fa61bc
Aaron M Joseph <125889154+aaronmj7@users.noreply.github.com>
2023-03-31 23:38:51 +0100

    Add files via upload

--- Assignment 2.py
+++ Assignment 2.py
@@ -23,30 +23,42 @@ def dataframe(file_name, countries, years):
     return df, df_t
 
 
+def stat(df):
+    print(df.describe())
+    print("\nSkewness:\n", skew(df))
+    print("\nKurtosis:\n", kurtosis(df))
+    return
+
+
 countries = ["India", "Japan", "Australia", "China", "United States",
              "Russian Federation", "United Kingdom"]
 years = [str(i) for i in range(1990, 2016)]
+
 df, df_t = dataframe("electricity from oil,gas,coal.csv", countries, years)
+co, co_t = dataframe("co2 emission per capita.csv", countries, years)
+
+stat(df_t)
+stat(co_t)
 
 df_t.plot()
 plt.show()
 
-co, co_t = dataframe("co2 emission per capita.csv", countries, years)
 co_t.plot()
 plt.show()
 
 years = [str(i) for i in range(1990, 2016, 5)]
 
 nu, nu_t = dataframe("electricity from nuclear.csv", countries, years)
+
+stat(nu_t)
+
 nu.plot.bar()
 plt.show()
 
-rn, rn_t = dataframe("Electricity from renewable ex hydro.csv", countries,
-                     years)
+rn, rn_t = dataframe("Electricity from renewable ex hydro.csv",
+                     countries, years)
 hy, hy_t = dataframe("Electricity from hydro.csv", countries, years)
+
+stat(rn_t.add(hy_t))
 rn.add(hy).plot.bar()
 plt.show()
-
-print(df_t.describe())
-print(skew(df_t))
-print(skew(co_t))
