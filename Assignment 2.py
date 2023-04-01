029a0bfa10cd47756bbd49420242cdaf36540c3f
Aaron M Joseph <125889154+aaronmj7@users.noreply.github.com>
2023-04-02 00:55:16 +0100

    Add files via upload

--- Assignment 2.py
+++ Assignment 2.py
@@ -5,6 +5,7 @@ Created on Mon Mar 13 00:25:50 2023
 @author: aaron
 """
 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from stats import skew, kurtosis
@@ -17,7 +18,8 @@ def dataframe(file_name, countries, years):
     df.dropna(how='any', thresh=100, axis=1, inplace=True)
     df.set_index(['Country Name'], inplace=True)
     df.index.rename("Country", inplace=True)
-    df.columns.name = "Years"
+    #df.columns = pd.to_datetime(df.columns)
+    df.columns.name = "Year"
     df_t = df.transpose()
     df_t.dropna(how='all', axis=1, inplace=True)
     df = df.loc[countries, years]
@@ -32,8 +34,18 @@ def stat(df):
     return
 
 
-countries = ["Belgium", "Denmark", "Finland", "Japan", "United Kingdom",
-             "United States"]
+def plotdf(df, name):
+    ax = df.plot(subplots=True, figsize=(8, 9), fontsize=14)
+    ax[0].set_title(name, fontsize=20, fontweight='bold')
+    ax[-1].set_xlabel(str(df.index.name), fontsize=20)
+    for i in range(0, len(ax)):
+        ax[i].legend(fontsize=11.5)
+    plt.tight_layout()
+    plt.show()
+
+
+countries = ["Belgium", "Denmark", "Finland", "India", "Japan", "South Africa",
+             "United Kingdom", "United States"]
 years = [str(i) for i in range(1994, 2015)]
 
 df, df_t = dataframe("electricity from oil,gas,coal.csv", countries, years)
@@ -42,19 +54,17 @@ co, co_t = dataframe("co2 emission per capita.csv", countries, years)
 stat(df_t)
 stat(co_t)
 
-df_t.plot(subplots=True)
-plt.show()
+plotdf(df_t, "Electicity Produced From oil, gas, coal (% of total)")
 
-co_t.plot(subplots=True)
-plt.show()
-'''
+plotdf(co_t, "CO2 Emmission per capita")
+
 years = [str(i) for i in range(1994, 2015, 5)]
 
 nu, nu_t = dataframe("electricity from nuclear.csv", countries, years)
 
 stat(nu_t)
 
-nu.plot.bar()
+nu.plot.bar(rot=35)
 plt.show()
 
 rn, rn_t = dataframe("Electricity from renewable ex hydro.csv",
@@ -63,6 +73,5 @@ hy, hy_t = dataframe("Electricity from hydro.csv", countries, years)
 
 stat(rn_t.add(hy_t))
 
-rn.add(hy).plot.bar()
+rn.add(hy).plot.bar(rot=35)
 plt.show()
-'''
